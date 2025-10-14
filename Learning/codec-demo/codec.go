c63ad97350c165957d147a6c92795cae988e65e0
xiaosansh <227628508@qq.com>
2025-10-14 15:17:08 +0800

    feat(codec): 抽象编解码模块，引入 Codec 接口
    将数据序列化/反序列化的逻辑从服务端的网络处理中抽离，封装为统一的 Codec 模块。
    
    这使得 RPC 框架的核心逻辑与具体的编码协议（如 JSON）解耦。未来可以轻松地通过实现 Codec 接口来增加对 Gob、Protobuf 等其他编码格式的支持，极大地提高了框架的可扩展性。

---
+++ Learning/codec-demo/codec.go
@@ -0,0 +1,50 @@
+package main
+
+import(
+  "encoding/json"
+  "io"
+)
+
+type Request struct{
+  ServerMethod string
+  Req uint64
+  Argv any
+}
+
+type Response struct{
+  Req uint64
+  Error string
+  Reply any
+}
+
+type Codec interface{
+  ReadRequest(*Request) error
+  WriteResponse(*Response) error
+  Close() error
+}
+
+type JSONCodec struct{
+  conn io.ReadWriteCloser
+  dec *json.Decoder
+  enc *json.Encoder
+}
+
+func NewJSONCodec(conn io.ReadWriteCloser) *JSONCodec {
+  return &JSONCodec{
+    conn: conn,
+    dec: json.NewDecoder(conn),
+    enc: json.NewEnconder(conn),
+  }
+}
+
+func(j *JSONCodec) ReadRequest(req *Request) error{
+  return j.dec.Decode(&req)
+}
+
+func(j *JSONCodec) WriteResponse(resp *Response) error{
+  return j.enc.Encode(resp)
+}
+
+func(j *JSONCodec) Close() error{
+  return j.conn.Close()
+}
